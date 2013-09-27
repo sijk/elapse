sijk/elapse

commit 83f390e67e647611f025954cf2c563df2cd358d2

Make DataSource a plugin interface
and move existing DataSources to a plugin.

include/plugin.h
@@ -3,6 +3,7 @@
 
 #include <QtPlugin>
 #include <QStringList>
+#include "datasource.h"
 
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * */
@@ -55,5 +56,14 @@ T *BasePlugin<T>::create(const QString &key)
 }
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+typedef FactoryInterface<DataSource> DataSourceInterface;
+Q_DECLARE_INTERFACE(DataSourceInterface, DataSourceInterface_iid)
+
+class DataSourcePlugin : public QObject, public BasePlugin<DataSource>
+{
+    Q_OBJECT
+    Q_INTERFACES(DataSourceInterface)
+};
 
 #endif // PLUGIN_H

include/datasource.h
@@ -29,4 +29,6 @@ protected:
     QString _host;
 };
 
+#define DataSourceInterface_iid "org.nzbri.elapse.DataSourceInterface"
+
 #endif // DATASOURCE_H

src/app/app.pro
@@ -1,6 +1,6 @@
 include(../../global.pri)
 
-QT           += core gui widgets network
+QT           += core gui widgets
 CONFIG       += qwt
 
 TARGET        = elapse
@@ -10,13 +10,9 @@ DESTDIR       = ../../
 
 SOURCES      += main.cpp \
                 mainwindow.cpp \
-                plugindialog.cpp \
-                tcpclienteegdatasource.cpp
+                plugindialog.cpp
 HEADERS      += mainwindow.h \
                 plugindialog.h \
-                tcpclienteegdatasource.h
-HEADERS      += dataprovider.h \
-                eegdecoder.h \
                 datasource.h
 FORMS        += mainwindow.ui \
                 plugindialog.ui

src/app/main.cpp
@@ -1,6 +1,6 @@
 #include <QApplication>
 #include "pluginloader.h"
-#include "tcpclienteegdatasource.h"
+#include "datasource.h"
 #include "eegdecoder.h"
 #include "mainwindow.h"
 
@@ -9,28 +9,35 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    TcpClientEegDataSource source;
+    PluginLoader loader;
+    MainWindow w(loader);
+    DataSource *source;
     EegDecoder eegdec;
 
-    source.setProperty("host", "overo.local");
+    source = loader.create<DataSource*>("TcpClientEegDataSource");
+    if (!source) {
+        qDebug() << "Error loading DataSource";
+        return 1;
+    }
+
+    source->setProperty("host", "overo.local");
     eegdec.setProperty("gain", 1);
     eegdec.setProperty("vref", 4.5e6);
 
     // Connect EEG signal chain
-    QObject::connect(&source, SIGNAL(eegReady(QByteArray)),
+    QObject::connect(source, SIGNAL(eegReady(QByteArray)),
                      &eegdec, SLOT(onData(QByteArray)));
     QObject::connect(&eegdec, SIGNAL(newSample(EegSample)),
                      &w, SLOT(onDataReady(EegSample)));
 
     // Connect controls to source
-    QObject::connect(&w, SIGNAL(start()), &source, SLOT(start()));
-    QObject::connect(&w, SIGNAL(stop()), &source, SLOT(stop()));
+    QObject::connect(&w, SIGNAL(start()), source, SLOT(start()));
+    QObject::connect(&w, SIGNAL(stop()), source, SLOT(stop()));
 
     // Connect source signals to GUI
-    QObject::connect(&source, SIGNAL(error(QString)),
+    QObject::connect(source, SIGNAL(error(QString)),
                      &w, SLOT(onSourceError(QString)));
-    QObject::connect(&source, SIGNAL(started()),
+    QObject::connect(source, SIGNAL(started()),
                      &w, SLOT(onSourceStarted()));
 
     w.show();

src/app/mainwindow.cpp
@@ -10,7 +10,7 @@
 
 #include <QDebug>
 
-MainWindow::MainWindow(QWidget *parent) :
+MainWindow::MainWindow(PluginLoader &loader, QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow),
     loader(loader),

src/app/mainwindow.h
@@ -17,7 +17,7 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
+    explicit MainWindow(PluginLoader &loader, QWidget *parent = 0);
     ~MainWindow();
 
 signals:

src/app/plugindialog.cpp
@@ -34,6 +34,7 @@ PluginDialog::PluginDialog(PluginLoader &loader, QWidget *parent) :
             foreach (QString key, loader.keysForFile(file)) {
                 auto keyItem = new QTreeWidgetItem(fileItem);
                 keyItem->setText(0, key);
+                keyItem->setFirstColumnSpanned(true);
             }
         }
 

src/plugins/eegsources/eegsources.pro
@@ -1,0 +1,21 @@
+include(../../../global.pri)
+
+# Generic plugin settings
+TEMPLATE        = lib
+CONFIG         += plugin
+QT             += network
+DESTDIR         = ../../../plugins/
+
+# Plugin-specific settings
+HEADERS         = tcpclienteegdatasource.h \
+                  udpeegdatasource.h \
+                  threadedtcpclienteegdatasource.h \
+                  eegthread.h \
+                  datasource.h \
+                  eegsourcesplugin.h \
+                  plugin.h
+SOURCES         = tcpclienteegdatasource.cpp \
+                  udpeegdatasource.cpp \
+                  threadedtcpclienteegdatasource.cpp \
+                  eegthread.cpp
+TARGET          = $$qtLibraryTarget(eegsourcesplugin)

commit 76f8360aebe0b725ff8fe3d1cba4620a6f624468

Refactor plugin.h to fix PluginLoader tests
83f390e broke the PluginLoaderTest suite. This commit fixes it by separating the
declaration of plugin base classes from the declaration of BasePlugin<>. The
headers for the test plugins were restructured to match.

include/plugin.h
@@ -1,61 +1,8 @@
 #ifndef PLUGIN_H
 #define PLUGIN_H
 
-#include <QtPlugin>
-#include <QStringList>
+#include "plugin_base.h"
 #include "datasource.h"
-
-
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
-template<class T>
-class FactoryInterface
-{
-public:
-    virtual ~FactoryInterface() {}
-
-    virtual QStringList keys() = 0;
-    virtual T *create(const QString &key) = 0;
-};
-
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
-template<class T>
-class BasePlugin : public FactoryInterface<T>
-{
-public:
-    virtual ~BasePlugin() {}
-
-    QStringList keys();
-    T *create(const QString &key);
-
-protected:
-    typedef QList<QMetaObject> ClassList;
-    virtual ClassList classes() = 0;
-};
-
-template<class T>
-QStringList BasePlugin<T>::keys()
-{
-    QStringList keys;
-    foreach (QMetaObject cls, classes())
-        keys << cls.className();
-    return keys;
-}
-
-template<class T>
-T *BasePlugin<T>::create(const QString &key)
-{
-    QString lkey = key.toLower();
-    foreach (QMetaObject cls, classes()) {
-        if (lkey == QString(cls.className()).toLower()) {
-            return qobject_cast<T*>(cls.newInstance());
-        }
-    }
-    return 0;
-}
-
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 typedef FactoryInterface<DataSource> DataSourceInterface;
 Q_DECLARE_INTERFACE(DataSourceInterface, DataSourceInterface_iid)

include/plugin_base.h
@@ -1,0 +1,59 @@
+#ifndef PLUGINBASE_H
+#define PLUGINBASE_H
+
+#include <QtPlugin>
+#include <QStringList>
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+template<class T>
+class FactoryInterface
+{
+public:
+    virtual ~FactoryInterface() {}
+
+    virtual QStringList keys() = 0;
+    virtual T *create(const QString &key) = 0;
+};
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+template<class T>
+class BasePlugin : public FactoryInterface<T>
+{
+public:
+    virtual ~BasePlugin() {}
+
+    QStringList keys();
+    T *create(const QString &key);
+
+protected:
+    typedef QList<QMetaObject> ClassList;
+    virtual ClassList classes() = 0;
+};
+
+template<class T>
+QStringList BasePlugin<T>::keys()
+{
+    QStringList keys;
+    foreach (QMetaObject cls, classes())
+        keys << cls.className();
+    return keys;
+}
+
+template<class T>
+T *BasePlugin<T>::create(const QString &key)
+{
+    QString lkey = key.toLower();
+    foreach (QMetaObject cls, classes()) {
+        if (lkey == QString(cls.className()).toLower()) {
+            return qobject_cast<T*>(cls.newInstance());
+        }
+    }
+    return 0;
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#endif // PLUGINBASE_H

src/pluginloader/pluginloader.cpp
@@ -4,7 +4,7 @@
 #include <QSet>
 #include <QStringList>
 #include "pluginloader.h"
-#include "plugin.h"
+#include "plugin_base.h"
 
 
 /*!

src/pluginloader/pluginloader.pro
@@ -8,4 +8,4 @@ CONFIG  += staticlib
 
 SOURCES += pluginloader.cpp
 HEADERS += pluginloader.h \
-           plugin.h
+           plugin_base.h

test/test_plugins/consumer.h
@@ -2,7 +2,6 @@
 #define DATACONSUMER_H
 
 #include <QObject>
-#include "plugin.h"
 
 class Consumer : public QObject
 {
@@ -14,16 +13,6 @@ public slots:
     virtual void onData(double data) = 0;
 };
 
-
 #define ConsumerInterface_iid "org.nzbri.elapse.test.ConsumerInterface"
-
-typedef FactoryInterface<Consumer> ConsumerInterface;
-Q_DECLARE_INTERFACE(ConsumerInterface, ConsumerInterface_iid)
-
-class ConsumerPlugin : public QObject, public BasePlugin<Consumer>
-{
-    Q_OBJECT
-    Q_INTERFACES(ConsumerInterface)
-};
 
 #endif // DATACONSUMER_H

test/test_plugins/cosineplugin/cosineplugin.h
@@ -1,7 +1,7 @@
 #ifndef COSINEPLUGIN_H
 #define COSINEPLUGIN_H
 
-#include "plugin.h"
+#include "test_plugins.h"
 #include "cosineproducer.h"
 
 class CosinePlugin : public ProducerPlugin

test/test_plugins/cosineplugin/cosineplugin.pro
@@ -9,7 +9,7 @@ DESTDIR         = ../
 HEADERS         = cosineproducer.h \
                   producer.h \
                   cosineplugin.h \
-                  plugin.h
+                  test_plugins.h
 SOURCES         = cosineproducer.cpp
 TARGET          = $$qtLibraryTarget(cosineplugin)
 

test/test_plugins/loggerplugin/loggerplugin.h
@@ -1,7 +1,7 @@
 #ifndef LOGGERCONSUMERPLUGIN_H
 #define LOGGERCONSUMERPLUGIN_H
 
-#include "plugin.h"
+#include "test_plugins.h"
 #include "loggerconsumer.h"
 
 class LoggerPlugin : public ConsumerPlugin

test/test_plugins/loggerplugin/loggerplugin.pro
@@ -9,7 +9,7 @@ DESTDIR         = ../
 HEADERS         = loggerconsumer.h \
                   consumer.h \
                   loggerplugin.h \
-                  plugin.h
+                  test_plugins.h
 SOURCES         = loggerconsumer.cpp
 TARGET          = $$qtLibraryTarget(loggerplugin)
 

test/test_plugins/producer.h
@@ -2,7 +2,6 @@
 #define DATAPROVIDER_H
 
 #include <QObject>
-#include "plugin.h"
 
 class Producer : public QObject
 {
@@ -18,16 +17,6 @@ public slots:
     virtual void stop() = 0;
 };
 
-
 #define ProducerInterface_iid "org.nzbri.elapse.test.ProducerInterface"
-
-typedef FactoryInterface<Producer> ProducerInterface;
-Q_DECLARE_INTERFACE(ProducerInterface, ProducerInterface_iid)
-
-class ProducerPlugin : public QObject, public BasePlugin<Producer>
-{
-    Q_OBJECT
-    Q_INTERFACES(ProducerInterface)
-};
 
 #endif // DATAPROVIDER_H

test/test_plugins/sineplugin/sineplugin.h
@@ -1,7 +1,7 @@
 #ifndef SINEPLUGIN_H
 #define SINEPLUGIN_H
 
-#include "plugin.h"
+#include "test_plugins.h"
 #include "tcpproducer.h"
 #include "sineproducer.h"
 

test/test_plugins/sineplugin/sineplugin.pro
@@ -11,7 +11,7 @@ HEADERS         = tcpproducer.h \
                   sineproducer.h \
                   producer.h \
                   sineplugin.h \
-                  plugin.h
+                  test_plugins.h
 SOURCES         = tcpproducer.cpp \
                   sineproducer.cpp
 TARGET          = $$qtLibraryTarget(sineplugin)

test/test_plugins/test_plugins.h
@@ -1,0 +1,26 @@
+#ifndef TEST_PLUGINS_H
+#define TEST_PLUGINS_H
+
+#include "plugin_base.h"
+#include "producer.h"
+#include "consumer.h"
+
+typedef FactoryInterface<Producer> ProducerInterface;
+Q_DECLARE_INTERFACE(ProducerInterface, ProducerInterface_iid)
+
+class ProducerPlugin : public QObject, public BasePlugin<Producer>
+{
+    Q_OBJECT
+    Q_INTERFACES(ProducerInterface)
+};
+
+typedef FactoryInterface<Consumer> ConsumerInterface;
+Q_DECLARE_INTERFACE(ConsumerInterface, ConsumerInterface_iid)
+
+class ConsumerPlugin : public QObject, public BasePlugin<Consumer>
+{
+    Q_OBJECT
+    Q_INTERFACES(ConsumerInterface)
+};
+
+#endif // TEST_PLUGINS_H