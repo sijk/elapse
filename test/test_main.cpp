a2911ec6dd413d9fadc899df7f3bbe57a60693a1
Simon Knopp <simon.knopp@pg.canterbury.ac.nz>
2014-03-25 12:07:50 +1300

    Improve test_main
    Create QApp/QGuiApp/QCoreApp as appropriate.

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,18 +1,20 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-#ifdef QT_GUI_LIB
-#include <QApplication>
+#if defined(QT_WIDGETS_LIB)
+#  include <QApplication>
+#  define Application QApplication
+#elif defined(QT_GUI_LIB)
+#  include <QGuiApplication>
+#  define Application QGuiApplication
 #else
-#include <QCoreApplication>
+#  include <QCoreApplication>
+#  define Application QCoreApplication
 #endif
 
-int main(int argc, char** argv) {
-#ifdef QT_GUI_LIB
-    QApplication app(argc, argv);
-#else
-    QCoreApplication app(argc, argv);
-#endif
+int main(int argc, char** argv)
+{
+    Application app(argc, argv);
     testing::InitGoogleMock(&argc, argv);
     return RUN_ALL_TESTS();
 }
