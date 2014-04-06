ac91c8f3fa63904a4863d03aa59de59dd9ba1f0a
Simon Knopp <simon.knopp@pg.canterbury.ac.nz>
2014-04-07 11:53:56 +1200

    Add some test utilities
    - Pretty-print QVariant
    - Scoped QxtLogger output suppressor

--- test/test_utils.h
+++ test/test_utils.h
@@ -3,13 +3,50 @@
 
 #include <iostream>
 #include <QString>
+#include <QVariant>
+#include <QDebug>
 #include <QDir>
+#include <QxtLogger>
 
+
+template<class T>
+void qDebugPrintTo(const T &value, std::ostream *os)
+{
+    QString s;
+    QDebug(&s) << value;
+    *os << s.toStdString();
+}
 
 void PrintTo(const QString& str, std::ostream* os)
 {
     *os << "QString(\"" << str.toStdString() << "\")";
 }
+
+void PrintTo(const QVariant& var, std::ostream* os)
+{
+    qDebugPrintTo(var, os);
+}
+
+
+class SuppressLogging
+{
+public:
+    SuppressLogging()
+    {
+        enabledEngines = qxtLog->allEnabledLoggerEngines();
+        foreach (const QString &engine, enabledEngines)
+            qxtLog->disableLoggerEngine(engine);
+    }
+
+    ~SuppressLogging()
+    {
+        foreach (const QString &engine, enabledEngines)
+            qxtLog->enableLoggerEngine(engine);
+    }
+
+private:
+    QStringList enabledEngines;
+};
 
 
 bool recursiveRmDir(const QString &dirname)
