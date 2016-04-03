ceed75c1ce51667f1216b36f1bc5f1dbf6af10cf
Simon Knopp <simon.knopp@pg.canterbury.ac.nz>
2016-04-03 15:48:01 +1300

    Add license info (GPLv3+ with plugin exception)

--- app/lib/elements/featurextractor_p.h
+++ app/lib/elements/featurextractor_p.h
@@ -1,3 +1,28 @@
+/*
+ * Elapse Client
+ * Copyright (C) 2015 Simon Knopp
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ *
+ * The licensors of this Program grant you additional permission to link
+ * this Program with other modules by means of the provided plugin
+ * interface and to convey the resulting work, regardless of the license
+ * terms of the other modules.
+ */
+
 #ifndef FEATUREXTRACTOR_P_H
 #define FEATUREXTRACTOR_P_H
 
--- app/src/elapseclient.cpp
+++ app/src/elapseclient.cpp
@@ -1,3 +1,28 @@
+/*
+ * Elapse Client
+ * Copyright (C) 2015 Simon Knopp
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ *
+ * The licensors of this Program grant you additional permission to link
+ * this Program with other modules by means of the provided plugin
+ * interface and to convey the resulting work, regardless of the license
+ * terms of the other modules.
+ */
+
 #include <QStateMachine>
 #include <QSignalTransition>
 #include <QMessageBox>
@@ -19,7 +44,7 @@ const char* DEFAULT_ADDR = "192.168.2.2";
 
 const char* ABOUT_TEXT = R"""(
 <h3>Elapse Client</h3>
-<p>&copy; NeuroTech, 2014</p>
+<p>&copy; NeuroTech, 2015</p>
 <p>
 The Elapse platform is a real-time cognitive monitoring system. This
 application, the client, receives and processes data from the Elapse device.
